2606d5187a83a6f06235f283eb32f5c2de064083
Onyewuchi Emeka <marrnuel123@.com>
2023-07-29 19:02:34 +0100

    ()chore: set up basic homescreen

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainerRef } from "@react-navigation/native";
+import MovieScreen from "./screens/MovieScreen";
+import { Provider } from "react-redux";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen ref name="Home" options={{headerShown:false}} component={HomeScreen} />
+        <Stack.Screen name="Movie" options={{headerShown:false}} component={MovieScreen} />
+  
+      </Stack.Navigator>
+    </NavigationContainer>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
